f97e354141d23cf94a3e11926badea68a970c5e2
Shinikie <Shinikie@users.noreply.github.com>
2019-12-31 00:08:11 +0700

    database

--- backend/functions.php
+++ backend/functions.php
@@ -1,9 +1,9 @@
 <?php
-$dbhost = "localhost";
-$dbport = 3307;
-$dbuser = "root";
-$dbpassword = "root";
-$dbname = "sms";
+$dbhost = "ec2-23-21-248-1.compute-1.amazonaws.com";
+$dbport = 5432;
+$dbuser = "fzqvsewfpuqchj";
+$dbpassword = "96e10f952004d3329102ea5a6ca8c573457a277edbfd202dac960770eee01306";
+$dbname = "dajgb2vte7s24a";
 $salt1 = "qm&h*";
 $salt2 = "!@#$%";
 //Connect to the DB
