ca5fc85e124f6ed5506a8413e5e762b917a6e211
Shinikie <Shinikie@users.noreply.github.com>
2019-12-31 23:00:23 +0700

    func

--- backend/functions.php
+++ backend/functions.php
@@ -1,19 +1,15 @@
 <?php
-$dburl="postgres://fzqvsewfpuqchj:96e10f952004d3329102ea5a6ca8c573457a277edbfd202dac960770eee01306@ec2-23-21-248-1.compute-1.amazonaws.com:5432/dajgb2vte7s24a
-";
-$dbhost = "ec2-23-21-248-1.compute-1.amazonaws.com";
-$dbport = 5432;
-$dbuser = "fzqvsewfpuqchj";
-$dbpassword = "96e10f952004d3329102ea5a6ca8c573457a277edbfd202dac960770eee01306";
-$dbname = "dajgb2vte7s24a";
+$dsn = "pgsql:"
+. "host=eec2-23-21-248-1.compute-1.amazonaws.com;"
+. "dbname=dajgb2vte7s24a;"
+. "user=fzqvsewfpuqchj;"
+. "port=5432;"
+. "sslmode=require;"
+. "password=96e10f952004d3329102ea5a6ca8c573457a277edbfd202dac960770eee01306";
+
+$db = new PDO($dsn);
 $salt1 = "qm&h*";
 $salt2 = "!@#$%";
-//Connect to the DB
-$connection = new mysqli($dburl,$dbhost, $dbuser, $dbpassword, $dbname, $dbport);
-if ($connection->connect_error) {
-    die ($connection->connect_error);
-}
-
 //this is used to execute all SQL queries
 function queryMysql($query) {
     global $connection;
